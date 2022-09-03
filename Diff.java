0c5948e1722dc5cb1e8f4c3839fef983e8c21cde
Avinash Sonawane <avinashsonawane55@gmail.com>
2022-09-03 16:29:40 +0530

    Adding Diff.java file

--- Diff.java
+++ Diff.java
@@ -1,3 +1,8 @@
 public class Diff{
-    
+        public static void main(String args[]){
+        int a=10;
+        int b=30;
+        int diff = a-b;
+        System.out.println("diff - "+diff);
+    }
 }
