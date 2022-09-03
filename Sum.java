89ed9daea2c8d5014bbd0134d517eb2635444cb2
Avinash Sonawane <avinashsonawane55@gmail.com>
2022-09-03 14:50:49 +0530

    changes added in sum.java file

--- Sum.java
+++ Sum.java
@@ -1,7 +1,7 @@
 public class Sum{
     public static void main(String args[]){
         int a=10;
-        int b=20;
+        int b=30;
         int sum = a+b;
         System.out.println("Sum - "+sum);
     }
